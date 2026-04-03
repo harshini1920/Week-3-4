eae46ab177affc6493b39b31c9396669e82a1283
harshini1920 <harshinisubramanian1901@gmail.com>
2026-04-03 21:01:04 +0530

    [Harshini] added: Java code for checking odd or even

---
+++ src/level2/q1.java
@@ -0,0 +1,27 @@
+package level2;
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even");
+                } else {
+                    System.out.println(i + " is odd");
+                }
+            }
+        } else {
+            System.out.println("The number " + number + " is not a natural number");
+        }
+
+        sc.close();
+    }
+
+}
