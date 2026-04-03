9dc1c41f0ad2d1134cc2f4fec7ca9d3a6e4bab43
harshini1920 <harshinisubramanian1901@gmail.com>
2026-04-03 21:26:42 +0530

    [Harshini] added: Java code for checking leap year

---
+++ src/level3/q1.java
@@ -0,0 +1,33 @@
+package level3;
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Invalid year. Please enter year >= 1582");
+        } else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println("Year is a Leap Year");
+                    } else {
+                        System.out.println("Year is not a Leap Year");
+                    }
+                } else {
+                    System.out.println("Year is a Leap Year");
+                }
+            } else {
+                System.out.println("Year is not a Leap Year");
+            }
+        }
+
+        sc.close();
+    }
+}
+
