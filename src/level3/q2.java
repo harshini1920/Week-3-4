9c50246d2cd5450c127346d55af8ab0cc8fb2158
harshini1920 <harshinisubramanian1901@gmail.com>
2026-04-03 21:33:13 +0530

    [Harshini] added: Java code for checking leap year using && operator

---
+++ src/level3/q2.java
@@ -0,0 +1,22 @@
+package level3;
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println("Year is a Leap Year");
+        } else if (year >= 1582) {
+            System.out.println("Year is not a Leap Year");
+        } else {
+            System.out.println("Invalid year. Please enter year >= 1582");
+        }
+
+        sc.close();
+    }
+}
